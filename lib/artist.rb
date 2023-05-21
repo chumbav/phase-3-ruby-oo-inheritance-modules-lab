0f14e8be48d2f6101930fab8d088f946681dba13
Victor <victor.chumba@student.moringaschool.com>
2023-05-22 00:27:04 +0300

    completed lab

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,11 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
   @@artists = []
 
   def initialize
